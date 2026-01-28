84b70b8d5af30d1f698c8089a635c92e37a738bf
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-28 18:01:28 +0200

    Create PageNotFound.jsx

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
+
+export default PageNotFound;
