64df46e2e575e822173bbf299979c6d8402eda22
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-28 18:01:27 +0200

    Create HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
+}
+
+export default HomePage;
