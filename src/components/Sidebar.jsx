8ec7683828037a3d5c0eab2b0adc8e54114fa156
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-29 17:11:36 +0200

    Create Sidebar.jsx

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import styles from "./SideBar.module.css";
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import { Outlet } from "react-router-dom";
+
+function SideBar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <Outlet />
+
+      {/* <p>List of cities</p> */}
+
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise{" "}
+          <strong>Gerges</strong>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default SideBar;
