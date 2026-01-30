56592b86cd61b68d04125c7609edb203e333d616
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-30 16:38:30 +0200

    Update Map.jsx

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,24 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>Map</h1>
+      <h1>Latitude: {lat}</h1>
+      <h1>Longitude: {lng}</h1>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 23 })}>
+        Clear search params
+      </button>
+    </div>
+  );
 }
 
 export default Map;
