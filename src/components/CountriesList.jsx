dfb9259f403a7442c53192ad4b08f73706a08f4c
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-30 16:02:51 +0200

    Update CountriesList.jsx

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -12,10 +12,16 @@ function CountriesList({ cities, isLoading }) {
       <Message message="Add your first city by clicking on a city on the map" />
     );
 
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
   return (
     <ul className={styles.countryList}>
-      {cities.map((country) => (
-        <CountryItem key={country.id} country={country} />
+      {countries.map((country, i) => (
+        <CountryItem key={i} country={country} />
       ))}
     </ul>
   );
