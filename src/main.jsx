3e9834787a861d3acffe71cb7050f66381164330
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-02-03 16:17:56 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
+
+// This will inject the font-face into your document
+polyfillCountryFlagEmojis();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
