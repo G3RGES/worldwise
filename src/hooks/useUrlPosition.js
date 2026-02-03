bf1f066455c02cc279ec09cd4f415d41e6d5d0cf
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-02-03 17:30:35 +0200

    Update useUrlPosition.js

--- src/hooks/useUrlPosition.js
+++ src/hooks/useUrlPosition.js
@@ -4,4 +4,6 @@ export function useUrlPosition() {
   const [searchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+
+  return [lat, lng];
 }
