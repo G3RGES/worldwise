a262f027531d4422b292836d5b06229a9856ad08
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-02-05 21:05:16 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,30 @@
 import { useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "./../components/PageNav";
+import Button from "../components/Button";
+import { useAuth } from "../context/FakeAuthContext";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
-  const [password, setPassword] = useState("qwerty");
+  const [email, setEmail] = useState("georgios.nashaat@gmail.com");
+  const [password, setPassword] = useState("12345678");
+
+  const { login } = useAuth();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (email && password) {
+      // TODO: login user
+      console.log(email, password);
+      login(email, password);
+    }
+  }
 
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -32,7 +46,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button className={styles.button}>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
